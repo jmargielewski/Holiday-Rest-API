3f4549929a355a2581bf2481b3128a38fea3bc5f
Jakub Margielewski <kuba.margielewski@gmail.com>
2017-08-05 17:36:28 +0200

    Search

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 $(function() {
-    let h1 = $('.test');
-    h1.addClass("color");
+
+
+    let wrapper = $('<div>', {class: 'wrapper'});
+    let app = $('<div>', {class: 'appSearch'});
+    let input = $('<input>', {class: 'inputSearch'});
+    let button = $('<button>', {class: 'buttonSearch'});
+
+    button.text('Search');
+
+    app.append(input);
+    app.append(button);
+    wrapper.append(app);
+    $('body').append(wrapper);
+
 });
