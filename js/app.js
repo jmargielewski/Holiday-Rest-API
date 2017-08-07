8110e70d70fd8b05fcfcdf5b3a214c04fb8f749b
Jakub Margielewski <kuba.margielewski@gmail.com>
2017-08-07 14:31:29 +0200

    change country

--- js/app.js
+++ js/app.js
@@ -1,34 +1,122 @@
-
 $(function() {
 
-    let urlAPI = 'https://holidayapi.com/v1/holidays?key=b8a9beec-2f3e-4319-b0f7-68ee562d49df&country=PL&year=2016&month=05';
+    const inputsDivBtn = $('.inputsDivBtn');
 
-    let sectionHolidays = $('.holidays');
-    let holidaysWrap = $('<div>', {class: 'holidaysWrap'});
-    let ul = $('<ul>', {class: 'holidaysList'});
+    let changeCountry = function (){
+        let countryInput = $('#inputText');
+        let monthInput = $('#inputMonth');
+        let userValueofInput = countryInput.val().toUpperCase();
+        let urlAPI = `https://holidayapi.com/v1/holidays?key=b8a9beec-2f3e-4319-b0f7-68ee562d49df&country=${userValueofInput}&year=2016&month=01`;
+        createList( urlAPI );
+    };
 
-    function createListHolidays( holidays ) {
-        for(let i = 0 ; i < holidays.length; i++) {
-            let li = $('<li>');
-            li.text(holidays[i].name + ' ' + holidays[i].date);
-            ul.append(li);
-            holidaysWrap.append(ul);
-            sectionHolidays.append(holidaysWrap);
-        };
+    let createList = function( urlAPI ){
+        let sectionHolidays = $('.holidays');
+        let holidaysWrap = $('<div>', {class: 'holidaysWrap'});
+        let ul = $('<ul>', {class: 'holidaysList'});
+
+        function createListHolidays( holidays ) {
+            for(let i = 0 ; i < holidays.length; i++) {
+                let li = $('<li>');
+                li.text(holidays[i].name + ' ' + holidays[i].date);
+                ul.append( li );
+                holidaysWrap.append( ul );
+                sectionHolidays.append( holidaysWrap );
+            };
+        }
+        function loadHolidays() {
+            $.ajax({
+                url: urlAPI
+            }).done( function( response ){
+                createListHolidays( response.holidays );
+            }).fail( function( error ) {
+                console.log( 'ERROR: ', error);
+            })
+        }
+        loadHolidays()
     }
 
-    function loadHolidays() {
-        $.ajax({
-            url: urlAPI
-        }).done( function( response ){
-            createListHolidays( response.holidays );
-        }).fail( function( error ) {
-            console.log( 'ERROR: ', error);
-        })
-    }
+    inputsDivBtn.on('click', changeCountry);
 
-    loadHolidays()
-
-    //////////////////////////
-
+    //predefinedCountryValues = [
+    //         "AR",
+    //         "AO",
+    //         "AT",
+    //         "AU",
+    //         "AW",
+    //         "AX",
+    //         "BA",
+    //         "BE",
+    //         "BG",
+    //         "BO",
+    //         "BR",
+    //         "BS",
+    //         "CA",
+    //         "CH",
+    //         "CN",
+    //         "CO",
+    //         "CR",
+    //         "CU",
+    //         "CZ",
+    //         "DE",
+    //         "DK",
+    //         "DO",
+    //         "EC",
+    //         "ES",
+    //         "FI",
+    //         "FR",
+    //         "GB",
+    //         "GB-ENG",
+    //         "GB-NIR",
+    //         "GB-SCT",
+    //         "GB-WLS",
+    //         "GR",
+    //         "GT",
+    //         "HK",
+    //         "HN",
+    //         "HR",
+    //         "HU",
+    //         "ID",
+    //         "IE",
+    //         "IN",
+    //         "IL",
+    //         "IS",
+    //         "IT",
+    //         "JP",
+    //         "KZ",
+    //         "LS",
+    //         "LU",
+    //         "MG",
+    //         "MQ",
+    //         "MU",
+    //         "MX",
+    //         "MZ",
+    //         "NG",
+    //         "NL",
+    //         "NO",
+    //         "PE",
+    //         "PK",
+    //         "PH",
+    //         "PL",
+    //         "PR",
+    //         "PT",
+    //         "PY",
+    //         "RE",
+    //         "RO",
+    //         "RU",
+    //         "SC",
+    //         "SE",
+    //         "SG",
+    //         "SI",
+    //         "ST",
+    //         "SK",
+    //         "TN",
+    //         "TR",
+    //         "UA",
+    //         "US",
+    //         "UY",
+    //         "VE",
+    //         "ZA",
+    //         "ZW",
+    //     ];
 });
